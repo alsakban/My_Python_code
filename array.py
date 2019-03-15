554cbbe2ebabbf84ebeda4d70475e48ba673e4ce
alsakban <43884797+alsakban@users.noreply.github.com>
2019-03-15 10:18:33 +0100

    Add files via upload

---
+++ array.py
@@ -0,0 +1,14 @@
+#  The Array module provide an array () object that store only homogenous data.
+
+from array import array
+
+a = array('H', [4000, 10 , 700, 22222, 5000,5555])
+sum(a)
+
+a[1:3]
+array('H', [10, 700])
+print (a)
+print (a[2:5])
+
+
+print (9999999999999999999999999999999999999 + 1)
